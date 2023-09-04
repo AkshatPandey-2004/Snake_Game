da6af63f817b2d99b130e6120dacc40bbf414d62
Akshat Pandey <akshatpandey26.2004@gmail.com>
2023-09-04 15:07:30 +0530

    Main Class Updated

--- Snake Game/snake_game.py
+++ Snake Game/snake_game.py
@@ -98,3 +98,66 @@ class FRUIT:
         self.x=random.randint(0,cell_number-1)
         self.y=random.randint(0,cell_number-1)
         self.pos=Vector2(self.x,self.y)
+
+class MAIN:
+   def __init__(self):
+       self.snake=SNAKE()
+       self.fruit=FRUIT()
+     
+  
+   def update(self):
+       self.snake.move_snake()
+       self.check_collision()
+       self.check_fails()
+
+   def draw_elements(self):
+    self.draw_grass()
+    self.fruit.draw_fruit()
+    self.snake.draw_snake()
+    self.draw_score()
+
+   def check_collision(self):
+       if self.fruit.pos == self.snake.body[0]:
+           self.fruit.randomize()
+           self.snake.add_block()
+           
+
+       for block in self.snake.body[1:]:
+           if block == self.fruit.pos:
+               self.fruit.randomize()   
+
+   def check_fails(self):
+       if not 0 <= self.snake.body[0].x <cell_number or not 0 <= self.snake.body[0].y <cell_number:
+           self.game_over()
+
+       for block in self.snake.body[1:]:
+           if block==self.snake.body[0]:
+               self.game_over()
+
+   def game_over(self):
+       print("\nSCORE: "+str(len(self.snake.body)-3))
+       pygame.quit()
+       sys.exit()
+
+   def draw_grass(self):
+       grass_color=(167,209,61)
+       for row in range(cell_number):
+           if row%2==0:
+               for col in range(cell_number):
+                   if col%2==0:
+                       grass_rect=pygame.Rect(col*cell_size,row*cell_size,cell_size,cell_size)
+                       pygame.draw.rect(screen,grass_color,grass_rect)
+           else:
+                for col in range(cell_number):
+                   if col%2!=0:
+                       grass_rect=pygame.Rect(col*cell_size,row*cell_size,cell_size,cell_size)
+                       pygame.draw.rect(screen,grass_color,grass_rect)
+   def draw_score(self):
+       score_text=str(len(self.snake.body)-3)
+       score_surface=game_font.render(score_text,True,(56,74,12))
+       score_x=int(cell_size*cell_number-60)
+       score_y=int(cell_size*cell_number-40)
+       score_rect=score_surface.get_rect(center=(score_x,score_y))
+       apple_rect=apple.get_rect(midright=(score_rect.left,score_rect.centery))
+       screen.blit(score_surface,score_rect)
+       screen.blit(apple,apple_rect)
